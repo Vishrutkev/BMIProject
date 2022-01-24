ea4da8c282d8fce98e4094ddb44aa1e1dd98f612
Vishrut kevadiya <vishrutk@my.yorku.ca>
2022-01-24 11:50:56 -0500

    Update BMIModelTest.java

--- app/src/test/java/ca/yorku/bmi/BMIModelTest.java
+++ app/src/test/java/ca/yorku/bmi/BMIModelTest.java
@@ -19,4 +19,23 @@ public class BMIModelTest {
         System.out.println(BMIModel.getBMI(weight, height));
 
     }
+    
+     @Test
+    public void toDoubleTest(){
+        System.out.println("Testing toDouble");
+        String num = "123.4";
+        double value = Double.parseDouble(num);
+        System.out.println(value);
+
+
+        }
+
+    @Test
+    public void formatBMITest(){
+        double hei = 12.37565;
+        hei = Math.round(hei*10.0)/10.0;
+        String str = String.valueOf(hei);
+        System.out.println(str);
+        
+    }
 }
